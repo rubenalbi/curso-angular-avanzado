1d9692c203cd07cd52b64d15ad56831b8cde06f7
Rubén Albiach <ruben.albiach@gmail.com>
2022-02-21 11:03:05 +0100

    Tema 1 y 2 Lazy loading y Transclusión

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RoutingModule } from './cmp01-lazy-loading/app.routes';
+import { Cmp01LazyLoadingComponent } from './cmp01-lazy-loading/cmp01-lazy-loading.component';
+import { InicioComponent } from './cmp01-lazy-loading/inicio/inicio.component';
+import { Cmp02TransclusionComponent } from './cmp02-transclusion/cmp02-transclusion.component';
+import { AcordeonComponent } from './cmp02-transclusion/acordeon/acordeon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Cmp01LazyLoadingComponent,
+    InicioComponent,
+    Cmp02TransclusionComponent,
+    AcordeonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
